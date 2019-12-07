fa8b7b1ec16d5da874f0990e71ff18ba3c725aeb
a935368322 <42330679+a935368322@users.noreply.github.com>
2019-12-07 20:21:46 +0800

    Update Query.cs

--- Kogel.Dapper.Extension.Test/UnitTest/Mssql/Query.cs
+++ Kogel.Dapper.Extension.Test/UnitTest/Mssql/Query.cs
@@ -17,7 +17,7 @@ namespace Kogel.Dapper.Extension.Test.UnitTest.Mssql
 {
     public class Query
     {
-        string mssqlConnection = "Data Source=42.157.195.21,4344;Initial Catalog=Qx_Sport_Common;User ID=qxdev;Password=qxdev123456;";
+        string mssqlConnection = "Data Source=localhost;Initial Catalog=Qx_Sport_Common;User ID=qxdev;Password=qxdev123456;";
         public void Test()
         {
 			//        using (var conn = new SqlConnection(mssqlConnection))
