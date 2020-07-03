aa10cf70950d3bb3e5dc69c51bb6530a4a6a40f9
Kogel <42330679+a935368322@users.noreply.github.com>
2020-07-03 20:27:39 +0800

    修复20200703版本更新后参数丢失问题
    修复20200703版本更新后参数丢失问题

--- Kogel.Dapper.Extension/Expressions/WhereExpression.cs
+++ Kogel.Dapper.Extension/Expressions/WhereExpression.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Expressions;
+using System.Linq.Expressions;
 using System.Text;
 using Dapper;
 using Kogel.Dapper.Extension.Core.Interfaces;
@@ -6,73 +6,73 @@ using System.Linq;
 
 namespace Kogel.Dapper.Extension.Expressions
 {
-	/// <summary>
-	/// 解析查询条件
-	/// </summary>
-	public sealed class WhereExpression : WhereExpressionVisitor
-	{
-		#region sql指令
-		private readonly StringBuilder _sqlCmd;
-		/// <summary>
-		/// sql指令
-		/// </summary>
-		public string SqlCmd { get; }
-		/// <summary>
-		/// 参数
-		/// </summary>
-		public new DynamicParameters Param { get; }
-		#endregion
-		/// <summary>
-		/// 解析条件对象
-		/// </summary>
-		/// <param name="expression">表达式</param>
-		/// <param name="prefix">参数标记</param>
-		/// <param name="providerOption"></param>
-		public WhereExpression(LambdaExpression expression, string prefix, SqlProvider provider) : base(provider)
-		{
-			this._sqlCmd = new StringBuilder(100);
-			this.Param = new DynamicParameters();
-			this.providerOption = provider.ProviderOption;
-			base.Prefix = prefix;
-			//开始解析对象
-			Visit(expression);
-			//开始拼接成条件
-			this._sqlCmd.Append(base.SpliceField);
-			this.SqlCmd = " AND " + this._sqlCmd.ToString();
-			this.Param.AddDynamicParams(base.Param);
-		}
+    /// <summary>
+    /// 解析查询条件
+    /// </summary>
+    public sealed class WhereExpression : WhereExpressionVisitor
+    {
+        #region sql指令
+        private readonly StringBuilder _sqlCmd;
+        /// <summary>
+        /// sql指令
+        /// </summary>
+        public string SqlCmd { get; }
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public new DynamicParameters Param { get; }
+        #endregion
+        /// <summary>
+        /// 解析条件对象
+        /// </summary>
+        /// <param name="expression">表达式</param>
+        /// <param name="prefix">参数标记</param>
+        /// <param name="providerOption"></param>
+        public WhereExpression(LambdaExpression expression, string prefix, SqlProvider provider) : base(provider)
+        {
+            this._sqlCmd = new StringBuilder(100);
+            this.Param = new DynamicParameters();
+            this.providerOption = provider.ProviderOption;
+            base.Prefix = prefix;
+            //开始解析对象
+            Visit(expression);
+            //开始拼接成条件
+            this._sqlCmd.Append(base.SpliceField);
+            this.SqlCmd = " AND " + this._sqlCmd.ToString();
+            this.Param.AddDynamicParams(base.Param);
+        }
 
-		/// <summary>
-		/// 解析二元表达式
-		/// </summary>
-		/// <param name="node"></param>
-		/// <returns></returns>
-		protected override Expression VisitBinary(BinaryExpression node)
-		{
-			var binaryWhere = new BinaryExpressionVisitor(node, base.Provider);
-			this._sqlCmd.Append(binaryWhere.SpliceField);
-			base.Param.AddDynamicParams(binaryWhere.Param);
-			return node;
-		}
+        /// <summary>
+        /// 解析二元表达式
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            var binaryWhere = new BinaryExpressionVisitor(node, base.Provider, 0, base.Prefix);
+            this._sqlCmd.Append(binaryWhere.SpliceField);
+            base.Param.AddDynamicParams(binaryWhere.Param);
+            return node;
+        }
 
-		/// <summary>
-		/// 解析!不等于(!里只能包含一个条件)
-		/// </summary>
-		/// <param name="node"></param>
-		/// <returns></returns>
-		protected override Expression VisitUnary(UnaryExpression node)
-		{
-			if (node.NodeType == ExpressionType.Not)
-			{
-				this._sqlCmd.Append(" Not (");
-				Visit(node.Operand);
-				this._sqlCmd.Append(") ");
-			}
-			else
-			{
-				Visit(node.Operand);
-			}
-			return node;
-		}
-	}
+        /// <summary>
+        /// 解析!不等于(!里只能包含一个条件)
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        protected override Expression VisitUnary(UnaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Not)
+            {
+                this._sqlCmd.Append(" Not (");
+                Visit(node.Operand);
+                this._sqlCmd.Append(") ");
+            }
+            else
+            {
+                Visit(node.Operand);
+            }
+            return node;
+        }
+    }
 }
