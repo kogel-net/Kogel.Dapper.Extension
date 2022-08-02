e3e863d315565ee5ba24b0710fc2706da59bd55c
An-nima <78138253+An-nima@users.noreply.github.com>
2022-08-02 10:35:12 +0800

    Update IAggregation.cs
    添加异步聚合函数。

--- Kogel.Dapper.Extension/Core/Interfaces/IAggregation.cs
+++ Kogel.Dapper.Extension/Core/Interfaces/IAggregation.cs
@@ -11,15 +11,25 @@ namespace Kogel.Dapper.Extension.Core.Interfaces
         /// </summary>
         /// <returns></returns>
         int Count();
-
-		/// <summary>
-		/// 总和
-		/// </summary>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="sumExpression"></param>
-		/// <returns></returns>
-		TResult Sum<TResult>(Expression<Func<T, TResult>> sumExpression);
-
+        /// <summary>
+        /// 行数
+        /// </summary>
+        /// <returns></returns>
+        Task<int> CountAsync();
+        /// <summary>
+        /// 总和
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sumExpression"></param>
+        /// <returns></returns>
+        TResult Sum<TResult>(Expression<Func<T, TResult>> sumExpression);
+        /// <summary>
+        /// 总和
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="sumExpression"></param>
+        /// <returns></returns>
+        Task<TResult> SumAsync<TResult>(Expression<Func<T, TResult>> sumExpression);
         /// <summary>
         /// 最小值
         /// </summary>
@@ -27,7 +37,13 @@ namespace Kogel.Dapper.Extension.Core.Interfaces
         /// <param name="minExpression"></param>
         /// <returns></returns>
         TResult Min<TResult>(Expression<Func<T, TResult>> minExpression);
-
+        /// <summary>
+        /// 最小值
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="minExpression"></param>
+        /// <returns></returns>
+        Task<TResult> MinAsync<TResult>(Expression<Func<T, TResult>> minExpression);
         /// <summary>
         /// 最大值
         /// </summary>
@@ -35,5 +51,12 @@ namespace Kogel.Dapper.Extension.Core.Interfaces
         /// <param name="maxExpression"></param>
         /// <returns></returns>
         TResult Max<TResult>(Expression<Func<T, TResult>> maxExpression);
+        /// <summary>
+        /// 最大值
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="maxExpression"></param>
+        /// <returns></returns>
+        Task<TResult> MaxAsync<TResult>(Expression<Func<T, TResult>> maxExpression);
     }
 }
